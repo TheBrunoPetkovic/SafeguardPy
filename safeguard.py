e712a677561a91cb7556f4d55ebea3990575d54b
TheBrunix <bruno.petkovic.business@gmail.com>
2024-03-15 12:47:27 +0100

    Popravljeno

--- safeguard.py
+++ safeguard.py
@@ -1,41 +1,34 @@
 import sys
 
-# Getting the arguments
-args = sys.argv[1:]
+def init(master_password_args):
+   print("Operacija - init")
 
-# Function that handles new initialisation
-def init():
-   pass
+def new_data(master_password_args, adress_args, password_args):
+   print("Operacija - new data")
 
-# Function that handles new pair of data - adress and password
-def new_data():
-   pass
-
-# Function that handles request for stored data - password
-def get_data():
-   pass
-
-# args[0] - Name of operation
-operation = args[0]
-# args[1] - Master Password
-master_password_args = args[1]
-# args[2] - If exists - Adress
-if len(args) >= 3:
-   adress = args[2]
-   # args[3] - If exists - Password
-   if len(args) == 4: 
-      password_args = args[3]
+def get_data(master_password_args, adress_args):
+   print("Operacija - get data")
 
 def main():
+   args = sys.argv[1:]
+
+   operation = args[0]
+   master_password_args = args[1]
+   if len(args) >= 3:
+      adress_args = args[2]
+      if len(args) == 4: 
+         password_args = args[3]
    
-   if args[0] == "init":
-      init()
+   if operation == "init":
+      init(master_password_args)
 
-   if args[0] == "put":
-      pass
+   if operation == "put":
+      new_data(master_password_args, adress_args, password_args)
 
-   if args[0] == "get":
-      pass
+   if operation == "get":
+      get_data(master_password_args, adress_args)
 
 if __name__ == "__main__":
    main()
+
+# DODAT UVJETE DA AKO JE NPR INIT DA MOZE BIT MAX 2 ARGUMENTA
